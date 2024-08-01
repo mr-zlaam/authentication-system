213f3cd40aa6ab773b6d0443faaf8d422d10e4e4
mr-zlaam <zlaam.dev@gmail.com>
2024-08-01 21:08:27 +0500

    Add environment variable and image remote patterns to Next.js config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    BACKEND_URI: process.env.NEXT_PUBLIC_BACKEND_URI_DEV,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
